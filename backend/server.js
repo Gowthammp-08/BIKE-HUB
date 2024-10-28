df7873c6171057ad676326f58516d7b2ecbde68f
Gowthammp-08 <153415918+Gowthammp-08@users.noreply.github.com>
2024-10-28 12:53:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '1234',//your password
+    password: process.env.DB_PASSWORD || '',//your password
     database: process.env.DB_NAME || 'bike_hub',//your database name
     connectionLimit: 10, 
 });
